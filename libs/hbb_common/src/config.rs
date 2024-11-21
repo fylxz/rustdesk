d238e5b55bb806bccf87b661c1402af792d69ee1
fylxz <32405780+fylxz@users.noreply.github.com>
2024-11-21 23:29:34 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.qjfyx.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "08744624261";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.abc.com"];
+pub const PUBLIC_RS_PUB_KEY: &str = "0123456789";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
